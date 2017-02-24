0e0e2017b94a6585dfe0fc5fe6b358a6dd776257
Jeterim <jeterim@hotmail.fr>
2017-02-24 15:39:40 +0100

    Ajout base client/serveur python3

---
+++ client.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import socket
+import time
+import getpass
+import hashlib
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 6262
+BUFFER_SIZE = 2048
+MENU = {"LS":"Liste un repertoire", "MV":"Se deplacer dans un repertoire", "EXE":"Executer une commande"}
+
+def run():
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+    print ("                    _ _           _ ")
+    print (" _ __ ___   ___  __| (_) ___ __ _| |")
+    print ("| '_ ` _ \ / _ \/ _` | |/ __/ _` | |")
+    print ("| | | | | |  __/ (_| | | (_| (_| | |")
+    print ("|_| |_| |_|\___|\__,_|_|\___\__,_|_|")
+    print ("")
+    print ("-----------------------------------------------------------------------")
+    print ("---- Bienvenue sur le service de consultation de dossiers medicaux ----")
+    print ("-----------------------------------------------------------------------")
+    print ("Pour y acceder veuillez vous identifier")
+    tentatives = 3
+    access = 0
+    while tentatives != 0:
+        #login = raw_input("# Login : ")
+        login = input("# Login : ")
+        passwd = getpass.getpass("# Mot de passe : ")
+        pswdhash = hashlib.sha1(passwd.encode('utf-8')).hexdigest() #hashage direct du passwd pour ne pas l'envoyer en clair
+        #try/except a faire
+        print(pswdhash)
+        #s.send("{};{}:{}".format("LOGIN", login, pswdhash))
+        s.send(bytes("{};{}:{}".format("LOGIN", login, pswdhash), 'utf-8'))
+        time.sleep(0.5)
+        data = s.recv(BUFFER_SIZE).decode('utf-8')
+        print ("Le serveur me donne : {}".format(data))
+        if data == "granted":
+            tentatives = 0
+            access = 1
+        else:
+            tentatives = tentatives - 1
+            print ("L'autentification a echouee il vous reste {} tentative(s)".format(tentatives))
+
+
+
+
+    val = ""
+    while val != "quit" and access == 1:
+        print ("\n-------------------")
+        for menu in MENU:
+            print ("+ {}       {}".format(menu, MENU[menu]))
+        print ("-------------------\nTappez quit pour quitter le client\n")
+        val = input("Tappez votre commande ")
+
+        if val == "LS":
+            print ("Ls a faire")
+            s.send(b"LS;NULL")
+            data = s.recv(BUFFER_SIZE).decode()
+            print ("Le serveur me donne : {}".format(data))
+            listFileS = data.split(", ")
+            for file in listFileS:
+                fileS = file.split(";")
+                print (fileS[0], fileS[1])
+        elif val == "OPEN":
+            print ("OPEN a faire")
+            fileO = input("Fichier a ouvrir : ")
+            s.send("OPEN;{}".format(fileO).encode())
+            data = s.recv(BUFFER_SIZE).decode()
+            print ("Le serveur me donne : {}".format(data))
+        else:
+            if val != "quit":
+                print ("Commande non reconnue")
+
+    print ("Fin du client")
+    s.close()
+
+if __name__ == "__main__":
+    run()
---
+++ multiserver.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+import os
+import socket
+import time
+import hashlib
+from threading import Thread
+#from SocketServer import ThreadingMixIn #Python 2
+from socketserver import ThreadingMixIn #Python 3
+
+data_dict = {"john" : {"password": "d6b4e84ee7f31d88617a6b60421451272ebf1a3a", "role": "Doctor", "lastCo": "1355563265.81"}};
+
+
+class ClientThread(Thread):
+
+    def __init__(self, ip, port):
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        print ("[+] New thread started for "+ip+":"+str(port))
+
+
+    def run(self):
+        while True:
+            data = conn.recv(2048).decode('utf-8')
+            if not data:
+                break
+            print ("received data:", data)
+            args = data.split(";")
+            if args[0] == "LOGIN":
+                auth = args[1].split(":")
+                print ("Login : {} Password : {}".format(auth[0], auth[1]))
+                successauth = 0
+                for user in data_dict:
+                    if user == auth[0] and data_dict[user]["password"] == auth[1]:
+                        print ("it's him")
+                        successauth = 1
+                        break
+                if successauth == 1:
+                    # Check proprement si le login/mdp est correct
+                    # Check si personne ne s'est connecte avec cet identifiant deja (utiliser une date de co ?)
+                    conn.send(b"granted")
+                else:
+                    conn.send(b"forbidden")
+                #conn.send(data)  # echo
+                time.sleep(0.5)
+            elif args[0] == "LS":
+                myDir = "multi"
+                lsList = []
+                for fileO in os.listdir(myDir):
+                    if os.path.isfile(os.path.join(myDir, fileO)):
+                        lsList.append("F;{}".format(fileO))
+                    else:
+                        lsList.append("R;{}/".format(fileO))
+
+                print (lsList)
+                conn.send(", ".join(lsList).encode())
+            elif args[0] == "OPEN":
+                myDir = "multi" #A mettre en place plus haut (instancier une seule fois)
+                openFile = args[1]
+                if os.path.isfile(os.path.join(myDir, openFile)):
+                    os.system("open {}/{}".format(myDir, openFile))
+                    conn.send(b"opening")
+            else:
+                print ("Echec action")
+
+TCP_IP = '0.0.0.0'
+TCP_PORT = 6262
+BUFFER_SIZE = 2048  # Normally 1024, but we want fast response
+
+
+tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcpsock.bind((TCP_IP, TCP_PORT))
+threads = []
+
+while True:
+    tcpsock.listen(4)
+    print ("Waiting for incoming connections...")
+    (conn, (ip, port)) = tcpsock.accept()
+    newthread = ClientThread(ip, port)
+    newthread.start()
+    threads.append(newthread)
+
+for t in threads:
+    t.join()
