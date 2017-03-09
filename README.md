1778c8b9761104d4c04ae61a9b010ae755354ebc
David <david.neyron@gmail.com>
2017-03-09 15:05:04 +0100

    preparation merge

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # projet-sysRes
 Projet Système et Réseau 3A 2017 à l'INSA CVL
+
+## Serveur
+### Mise en place
+- [X] Gestion de plusieurs clients (sans notion de concurrence) 
+- [ ] Mise en place dictionnaire de données (BDD personnes + historique de connexion)
+- [ ] Mise en place de la structure (reception des données entrantes, des arguments envoyés par le client etc...)
+
+### Dictionnaire de données
+- [ ] Personnes pouvant s'authentifier (login, mdp, poste?, droits, date de dernière co?)
+- [ ] Personnes étant authentifiés actuellement sur le système
+- [ ] ....
+
+### Authentification
+- [ ] Authentification simple (comparaison du couple login/mdp avec la BDD)
+- [ ] Vérification d'une connexion possible (pas deux même personnes sur le serveur)
+- [ ] Gestion déconnexion (retirer son login des personnes authentifiés sur le système)
+- [ ]
+
+### Fonctionnalités
+- [ ] ls
+- [ ] mv
+- [ ] exe
+- [ ] Gestion des droits (via ACLs)
+- [ ] Connexion chiffrée (clés pas en clair sur le poste client + certificat coté serveur) SSL
+- [ ] Flux graphiques ?
+- [ ] Creer des logs sur les actions effectués ?
+
+
+
+## Client
+### Mise en place
+- [X] Mise en place de l'interface de connexion
+- [ ] Mise en place de l'interface pour les actions
+- [ ] Chiffrement (clés)
+- [ ] Flux graphiques ?
+- [ ] Interface graphique ? 
+
+
+## BDD / Dict
+### Structure
+- Data
+    + Name
+        + Password
+        + Role
+        + Last connexion
+
+## Bilan 1ere séance
+- Gestion des autentifications autre que en dur
+- Posibilité d'avoir des actions supplémentaires pour certains roles pour ajouter ou modifier les personnes pouvant d'autentitfier
+- Voir comment gerer les identifiants/mdp autre que ne dur (BDD ?)
+- Gerer les outils et commandes dynamiques (ex vi) et comment gerer la relation client/serveur
--- multiserver.py
+++ multiserver.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
+import hashlib
 import os
 import socket
 import time
 import hashlib
-import ssl
+import acl
+from socketserver import ThreadingMixIn  # Python 3
 from threading import Thread
-#from SocketServer import ThreadingMixIn #Python 2
-from socketserver import ThreadingMixIn #Python 3
+from subprocess import Popen, PIPE, run
+import shlex
+import ssl
 
-data_dict = {"john" : {"password": "d6b4e84ee7f31d88617a6b60421451272ebf1a3a", "role": "Doctor", "lastCo": "1355563265.81"}};
+data_dict = {"john" : {"password": "d6b4e84ee7f31d88617a6b60421451272ebf1a3a", "role": "doctor", "lastCo": "1488482763.272476", "connected":False}};
+
+#Init Acl
+acli = acl.Acl()
+acli.build_acl('permissions.xml')
 
 
 class ClientThread(Thread):
@@ -17,52 +24,71 @@ class ClientThread(Thread):
         Thread.__init__(self)
         self.ip = ip
         self.port = port
-        print ("[+] New thread started for "+ip+":"+str(port))
-
+        self.username = ""
+        print("[+] New thread started for "+ip+":"+str(port))
 
     def run(self):
+        print("test acces : {}".format(acli.check_access('john', 'general', 'w')))
         while True:
-            data = connstream.recv(2048).decode('utf-8')
+            data = conn.recv(2048).decode('utf-8')
             if not data:
                 break
-            print ("received data:", data)
-            args = data.split(";")
-            if args[0] == "LOGIN":
-                auth = args[1].split(":")
-                print ("Login : {} Password : {}".format(auth[0], auth[1]))
-                successauth = 0
-                for user in data_dict:
-                    if user == auth[0] and data_dict[user]["password"] == auth[1]:
-                        print ("it's him")
-                        successauth = 1
-                        break
-                if successauth == 1:
-                    # Check proprement si le login/mdp est correct
-                    # Check si personne ne s'est connecte avec cet identifiant deja (utiliser une date de co ?)
-                    connstream.send(b"granted")
-                else:
-                    connstream.send(b"forbidden")
-                connstream.send(data.encode())  # echo
-                time.sleep(0.5)
-            elif args[0] == "LS":
-                myDir = "multi"
-                lsList = []
-                for fileO in os.listdir(myDir):
-                    if os.path.isfile(os.path.join(myDir, fileO)):
-                        lsList.append("F;{}".format(fileO))
-                    else:
-                        lsList.append("R;{}/".format(fileO))
+            print("received data:", data)
 
-                print (lsList)
-                connstream.send(", ".join(lsList).encode())
-            elif args[0] == "OPEN":
-                myDir = "multi" #A mettre en place plus haut (instancier une seule fois)
-                openFile = args[1]
-                if os.path.isfile(os.path.join(myDir, openFile)):
-                    os.system("open {}/{}".format(myDir, openFile))
-                    connstream.send(b"opening")
+            args = data.split(None)
+            if args[0] == "LOGIN":
+                self.connect(args)
+            elif args[0] == "LOGOUT": #Gestion de la deconnexion
+                self.manageConnexion()
             else:
-                print ("Echec action")
+                # TODO Check if dangerous command
+                run(args,
+                    stdout=conn.makefile('w'),
+                    stdin=conn.makefile('r'),
+                    stderr=conn.makefile('w'))
+
+    def run_command(self, process, args):
+        out, err = process.communicate(input=" ".join(args).encode())
+        print("{}; {}".format(out.decode('utf-8'), err))
+        conn.send(out)
+
+    def connect(self, args):
+        auth = args[1].split(":")
+        print("Login : {} Password : {}".format(auth[0], auth[1]))
+        successauth = 0
+        for user in data_dict:
+            if user == auth[0] and data_dict[user]["password"] == auth[1]:
+                print("it's him")
+                successauth = 1
+                break
+        if successauth == 1:
+            self.username = user
+            self.updateTime()
+            self.manageConnexion()
+            # Check proprement si le login/mdp est correct
+            # Check si personne ne s'est connecte avec cet identifiant deja (utiliser une date de co ?)
+            print(self.username)
+            conn.send(b"granted")
+        else:
+            conn.send(b"forbidden")
+        # conn.send(data)  # echo
+        time.sleep(0.5)
+
+    def updateTime(self):
+        for user in data_dict.keys():
+            if user == self.username:
+                print("Hello again")
+                print(data_dict[user]['lastCo'])
+                data_dict[user]['lastCo'] = time.time()
+                print("And now it's {}".format(data_dict[user]['lastCo']))
+
+    def manageConnexion(self):
+        for user in data_dict.keys():
+            if user == self.username:
+                print(data_dict[user]['connected'])
+                data_dict[user]['connected'] = not data_dict[user]['connected']
+                print("And now it's {}".format(data_dict[user]['connected']))
+
 
 TCP_IP = '0.0.0.0'
 TCP_PORT = 6262
@@ -79,9 +105,9 @@ threads = []
 
 while True:
     tcpsock.listen(4)
-    print ("Waiting for incoming connections...")
-    (conn, (ip, port)) = tcpsock.accept()
-    connstream = context.wrap_socket(conn,server_side=True)
+    print("Waiting for incoming connections...")
+    (connstream, (ip, port)) = tcpsock.accept()
+    conn = context.wrap_socket(connstream,server_side=True)
     newthread = ClientThread(ip, port)
     newthread.start()
     threads.append(newthread)
