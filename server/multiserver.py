7922ef4e26219c1c704557b72d55837db1ede03b
Jeterim <jeterim@hotmail.fr>
2017-03-10 11:54:57 +0100

    Transition dict -> shelve

--- server/multiserver.py
+++ server/multiserver.py
@@ -30,12 +30,10 @@ class ClientThread(Thread):
 
     def run(self):
         print("test acces : {}".format(acli.check_access('john', 'general', 'w')))
-        base = shelve.open('base', 'c')
-        base['foo'] = {'a': {'a1':'test','a2':'f'}, 'b': ['b1','b2']}
-        base.close()
-        base2 = shelve.open('base')
-        print(base2['foo']['a']['a1'])
-        base2.close()
+        #base = shelve.open('base', 'c')
+        #base['john'] = {"password": "d6b4e84ee7f31d88617a6b60421451272ebf1a3a", "role": "doctor", "lastCo": "1488482763.272476", "connected":False}
+        #base['johnA'] = {"password": "d6b4e84ee7f31d88617a6b60421451272ebf1a3a", "role": "admin", "lastCo": "1488482763.272476", "connected":False}
+        #base.close()
         while True:
             data = conn.recv(2048).decode('utf-8')
             if not data:
@@ -52,7 +50,9 @@ class ClientThread(Thread):
                     #fonction a appeler pour la creation d'une nouvelle personne (dict + acl)
                     conn.send(b"personne cree")
             elif args[0] == "LOGOUT": #Gestion de la deconnexion
-                self.manageConnexion()
+                base2 = shelve.open('base')
+                self.manageConnexion(base2)
+                base2.close()
             else:
                 # TODO Check if dangerous command
                 run(args,
@@ -69,39 +69,38 @@ class ClientThread(Thread):
         auth = args[1].split(":")
         print("Login : {} Password : {}".format(auth[0], auth[1]))
         successauth = 0
-        for user in data_dict:
-            if user == auth[0] and data_dict[user]["password"] == auth[1]:
-                print("it's him")
-                successauth = 1
-                break
+        base2 = shelve.open('base')
+        if auth[0] in base2 and base2[auth[0]]["password"] == auth[1]:
+            print("it's him")
+            successauth = 1
         if successauth == 1:
-            self.username = user
-            self.role = data_dict[user]['role']
-            self.updateTime()
-            self.manageConnexion()
+            print("tg")
+            self.username = auth[0]
+            self.role = base2[auth[0]]["role"]
+            self.updateTime(base2)
+            print('et la')
+            self.manageConnexion(base2)
             # Check proprement si le login/mdp est correct
             # Check si personne ne s'est connecte avec cet identifiant deja (utiliser une date de co ?)
             print(self.username)
-            conn.send("granted;{}".format(data_dict[user]['role']).encode())
+            conn.send("granted;{}".format(base2[auth[0]]["role"]).encode())
         else:
             conn.send(b"forbidden")
         # conn.send(data)  # echo
         time.sleep(0.5)
+        base2.close()
 
-    def updateTime(self):
-        for user in data_dict.keys():
-            if user == self.username:
-                print("Hello again")
-                print(data_dict[user]['lastCo'])
-                data_dict[user]['lastCo'] = time.time()
-                print("And now it's {}".format(data_dict[user]['lastCo']))
+    def updateTime(self, base2):
+        if self.username in base2:
+            print(base2[self.username]["lastCo"])
+            base2[self.username]["lastCo"] = time.time()
+            print("And now it's {}".format(base2[self.username]["lastCo"]))
 
-    def manageConnexion(self):
-        for user in data_dict.keys():
-            if user == self.username:
-                print(data_dict[user]['connected'])
-                data_dict[user]['connected'] = not data_dict[user]['connected']
-                print("And now it's {}".format(data_dict[user]['connected']))
+    def manageConnexion(self, base2):
+        if self.username in base2:
+            print(base2[self.username]["connected"])
+            base2[self.username]["connected"] = not base2[self.username]["connected"]
+            print("And now it's {}".format(base2[self.username]["connected"]))
 
 
 TCP_IP = '0.0.0.0'
