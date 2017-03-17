6bff971e1be421f7ee6c69635121993858a942b5
Quentin Laplanche <quentin.laplanche@outlook.fr>
2017-03-17 18:03:33 +0100

    Ajout script install

---
+++ server/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='Projet-Medical',
+      version='1.0',
+      description='Dossier medical en Python',
+      author='Jeremy Petit, David Neyron, Quentin Laplanche',
+      author_email='david.neyron@insa-cvl.fr, jeremy.petit@insa-cvl.fr, quentin.laplanche@insa-cvl.fr',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['src'],
+      )
